a0ffea1fd65523b7cb0518d9dee18172bd66645f
deleonjeremy <moragajeremy@gmail.com>
2019-11-25 15:15:08 -0600

    Added the albums controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'albums/index'
+  get 'albums/show'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   get '/artists', to: 'artists#index'
