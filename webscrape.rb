2919edf60ee756528145f24f7e5c88267bd4ed1f
deleonjeremy <moragajeremy@gmail.com>
2019-11-24 22:08:16 -0600

    Created a Webscrape file to test and check out what's in the Spotify data

--- webscrape.rb
+++ webscrape.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
+require 'rspotify'
+RSpotify.authenticate('b1e0dad39f0f453190203b7755550476', '1bfa8af723bf41eb9da8656336d862de')
 require 'open-uri'
+require 'json'
 
-puts open('https://api.spotify.com/v1/artists/0TnOYISbd1XYRBk9myaseg').read
+artist = RSpotify::Artist.find('7Ln80lUS6He07XvHI8qqHH')
+
+first_album_tracks = artist.albums.first.tracks
+
+first_album.each do |x|
+  puts x.name
+end
