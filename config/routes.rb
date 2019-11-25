9a396be5b5c0de8fc37d7f6e346763b8ef90ae95
deleonjeremy <moragajeremy@gmail.com>
2019-11-24 23:50:29 -0600

    Changed routing format and root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  get 'artists/index'
-  get 'artists/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/artists', to: 'artists#index'
+  get '/artists/:id', to: 'artists#show', id: /\d+/
+  root to: 'artists#index'
 end
