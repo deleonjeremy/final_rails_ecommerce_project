b904ca5fca404f4a077876753175fea110e84fb1
deleonjeremy <moragajeremy@gmail.com>
2019-11-25 15:21:54 -0600

    Configured routing for albums controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'albums/index'
-  get 'albums/show'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   get '/artists', to: 'artists#index'
   get '/artists/:id', to: 'artists#show', id: /\d+/
 
+  get '/albums', to: 'albums#index'
+  get '/albums/:id', to: 'artists#show', id: /\d+/
 
   root to: 'artists#index'
 end
