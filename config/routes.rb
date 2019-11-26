ade3fbfad232f7ecd0c49673a91881816e69f977
deleonjeremy <moragajeremy@gmail.com>
2019-11-26 03:23:17 -0600

    Added the songs controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'songs/index'
+  get 'songs/show'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   get '/artists', to: 'artists#index'
