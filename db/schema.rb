37ca039c7a96c286b1e8b63c3f4626b97da8b3a3
deleonjeremy <moragajeremy@gmail.com>
2019-11-26 18:29:35 -0600

    Added Pages model and controller and ran its migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_26_085922) do
+ActiveRecord::Schema.define(version: 2019_11_27_002456) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -74,6 +74,14 @@ ActiveRecord::Schema.define(version: 2019_11_26_085922) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string "name"
+    t.text "content"
+    t.string "permalink"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
