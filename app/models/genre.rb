f8ee5d423f9263a74107d6956f0e877a8fb8b9aa
deleonjeremy <moragajeremy@gmail.com>
2019-11-28 16:20:54 -0600

    Added validation to genre model

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,6 @@
 class Genre < ApplicationRecord
   belongs_to :album # There is a album_id FK in the Genre table
   belongs_to :artist # There is a artist_id FK in the Genre table
+
+  validates :name, presence: true
 end
