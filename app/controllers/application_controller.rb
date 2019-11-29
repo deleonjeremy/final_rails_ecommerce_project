2c4f0c1596339e49a4ddbc1a704395a7f022728b
deleonjeremy <moragajeremy@gmail.com>
2019-11-28 18:33:30 -0600

    Moved session cart to Application controller/ Moved it to the application view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  before_action :initialize_session
+  before_action :load_cart
+
+  def add_to_cart
+    id = params[:id].to_i
+    session[:cart] << id unless session[:cart].include?(id)
+    redirect_to root_path
+  end
+
+  def remove_from_cart
+    id = params[:id].to_i
+    session[:cart].delete(id)
+    redirect_to root_path
+  end
+
+  private
+
+  def initialize_session
+    session[:cart] ||= [] # Empty array to store products
+  end
+
+  def load_cart
+    @cart = Album.find(session[:cart])
+  end
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,11 +1,35 @@
 # frozen_string_literal: true
 
 class SongsController < ApplicationController
+  before_action :initialize_session
+  before_action :load_cart
   def index
     @songs = Song.all
   end
 
-  def show;
+  def show
     @song = Song.find(params[:id])
+  end
+
+  def add_to_cart
+    id = params[:id].to_i
+    session[:cart] << id unless session[:cart].include?(id)
+    redirect_to root_path
+  end
+
+  def remove_from_cart
+    id = params[:id].to_i
+    session[:cart].delete(id)
+    redirect_to root_path
+  end
+
+  private
+
+  def initialize_session
+    session[:cart] ||= [] # Empty array to store products
+  end
+
+  def load_cart
+    @cart = Album.find(session[:cart])
   end
 end
